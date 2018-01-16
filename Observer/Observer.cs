4edb008f59be0a1ee8a96029ffee3c600ceb06c6
Laimonas Sutkus <laimonas.sutkus@gmail.com>
2018-01-16 21:22:48 +0200

    Update Observer DP

--- Observer/Observer.cs
+++ Observer/Observer.cs
@@ -4,51 +4,6 @@ using System.Text;
 
 namespace Observer
 {
-    interface ISubjectPublisher
-    {
-        void Register(IObserverSubscriber o);
-        void Unregister(IObserverSubscriber o);
-        void NotifyObserverSubscriber();
-    }
-
-    class Subject : ISubjectPublisher
-    {
-        private List<IObserverSubscriber> _observers;
-        private double a, b, c;
-
-        public Subject()
-        {
-            _observers = new List<IObserverSubscriber>();
-        }
-
-        public void Register(IObserverSubscriber o)
-        {
-            _observers.Add(o);
-        }
-
-        public void Unregister(IObserverSubscriber o)
-        {
-            _observers.Remove(o);
-        }
-
-        public void NotifyObserverSubscriber()
-        {
-            foreach (var observerSubscriber in _observers)
-            {
-                observerSubscriber.Update(a, b, c);
-            }
-        }
-
-        public void ChangeValues()
-        {
-            a = 5;
-            b = 5;
-            c = 0;
-
-            NotifyObserverSubscriber();
-        }
-    }
-
     interface IObserverSubscriber
     {
         void Update(double a, double b, double c);
---
+++ Observer/Subject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer
+{
+    interface ISubjectPublisher
+    {
+        void Register(IObserverSubscriber o);
+        void Unregister(IObserverSubscriber o);
+        void NotifyObserverSubscriber();
+    }
+
+    class Subject : ISubjectPublisher
+    {
+        private List<IObserverSubscriber> _observers;
+        private double a, b, c;
+
+        public Subject()
+        {
+            _observers = new List<IObserverSubscriber>();
+        }
+
+        public void Register(IObserverSubscriber o)
+        {
+            _observers.Add(o);
+        }
+
+        public void Unregister(IObserverSubscriber o)
+        {
+            _observers.Remove(o);
+        }
+
+        public void NotifyObserverSubscriber()
+        {
+            ChangeValues();
+
+            // Now notify all observers that values have changed
+            foreach (var observerSubscriber in _observers)
+            {
+                observerSubscriber.Update(a, b, c);
+            }
+        }
+
+        public void ChangeValues()
+        {
+            // Values have changed
+            a = 5;
+            b = 5;
+            c = 0;
+        }
+    }
+}
